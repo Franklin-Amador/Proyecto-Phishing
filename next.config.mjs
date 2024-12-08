8528a6f90fb8f6611b95a8950e9abc05dfe227e8
Franklin Amador <joelamador55@gmail.com>
2024-12-08 00:27:27 -0600

    prueba 25

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        URL_TURSO: process.env.URL_TURSO,
+        TOKEN: process.env.TOKEN,
+    },
+};
 
 export default nextConfig;
